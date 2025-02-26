6210bff88e30efe25d7b3dbe29c099fd7faee693
DanielCendry <d.cendry@sente.pl>
2025-02-26 16:55:56 +0100

    operatory cz.2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
-let a = 15;
-let b = 3;
+let x = 2;
 
-// console.log(a+b);  ctrl + k + c  automatyczne komentowanie zaznaczonego 
-// console.log(a-b);                fragmentu kodu
-// console.log(a*b);
-// console.log(a/b);
-// console.log(a % 4);
-// console.log(a++);  inkrementacja
-// console.log(b--);  dekrementacja
-// console.log(+a); -> 15
-// console.log(-b); -> -3
-console.log(2===2); // -> true bo liczba ta sama i typ danych ten sam
-console.log(2==='2'); //-> false bo liczba ta sama a typ danych inny
-console.log(2=='2'); // -> true bo liczba ta sama, typ nie jest weryfikowany
-console.log(2==2); // -> true bo liczba ta sama, typ nie jest weryfikowany
-console.log(3!=='3');
-console.log(3!='3');
+console.log(x+=2);
+console.log(x-=2);
+console.log(x*=2);
+console.log(x/=2);
+
+let y = 10;
+console.log(y%=3);
+
+let z = -1;
+console.log('Przesunięcie w lewo będzie wynosiło: ' + (z<<1));
+console.log('Przesunięcie w prawo będzie wynosiło: ' + (z>>1));
+
+console.log('Przesunięcie w prawo z wypełnieniem: ' + (z>>>1));
