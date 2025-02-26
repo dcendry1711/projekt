7bbb69d2e3d7654aeba8b2d1732f11a751663569
DanielCendry <d.cendry@sente.pl>
2025-02-26 15:33:50 +0100

    zmienne i ich typy

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-document.getElementById('p').innerHTML = 'Hello World';
+let a = 4; // zmienna typu NUMBER
+console.log(typeof(a)); //typeOf umożliwia sprawdzenie typu danych
+let b = 'JavaScript'; // zmienna typu STRING
+console.log(typeof(b));
+let c = true; // zmienna typu BOOLEAN true lub false
+console.log(typeof(c));
+let d = null; // zmienna typu null - nic, brak wartości
+console.log(typeof(d));
+let f; // zmienna typu UNDEFINED, zmienna niezdefiniowana
+console.log(typeof(f));
