c39ad2932ff6a72e8ceb89404ceda3462808266d
DanielCendry <d.cendry@sente.pl>
2025-02-27 16:27:05 +0100

    petla FOR

--- js/script.js
+++ js/script.js
@@ -1,38 +1,12 @@
-//let x = 1;
-// switch (x){
-//     case 1:
-//     case 2:
-//     case 3:
-//         if(x<2){
-//             console.log('x wynosi 1');
-//         } else if (x==2) {
-//             console.log('x wynosi 2');
-//         } else {
-//             console.log('x wynosi 3');
-//         }
-//         //console.log('x wynosi 1, 2 lub 3');
-//         break;
-//     case 4:
-//         console.log('x wynosi 4');
-//         break;
-//     case 5:
-//         console.log('x wynosi 5');
-//         break;
-//     default:
-//         console.log('żadna z powyższych opcji nie została wybrana');
+// let a = 25; 
+// for (i = 0; a >= i; i++) {
+//     console.log(i);
 // }
 
-let x = 3;
-let y = 5;
-let z = 10;
+//wyswietlic na konsoli 10 kolejnych parzystych liczb, począwszy od 2
 
-//która z wartości jest największa (lepsza opcja bez zagnieżdżania)
-
-if (x>y && x>z) {
-    console.log('wartość x jest największa i wynosi ' + x);
-} else if (y>x && y>z) {
-    console.log('wartość y jest największa i wynosi ' + y);
-} else {
-    console.log('wartość z jest największa i wynosi ' + z);
+for (let i=2; i <=20 ; i++) {
+    if (i % 2 === 0){
+        console.log(i);
+    }
 }
-
