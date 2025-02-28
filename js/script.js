9d089d3bb958f7b09f3ac9c4233806bbfebe8063
DanielCendry <d.cendry@sente.pl>
2025-02-28 17:29:05 +0100

    peta dowhile

--- js/script.js
+++ js/script.js
@@ -1,20 +1,6 @@
-// let a = 1;
-// while (a <= 10){
-//     console.log(a);
-//     a++;
-// }
+let a = 1;
 
-//Tworzymy skrypt który obliczy sumę dwóch liczb. 
-// Liczby będą podane przez użytkownika. 
-// Algorytm będzie wyświetlał liczby do momentu aż ich suma będzie mniejsza od 0.
-
-// let a = 0 ;
-// let b = 0 ;
-let suma = 0;
-
-while (suma >= 0){
-    let a = parseInt(prompt('podaj pierwszą liczbę'));
-    let b = parseInt(prompt('podaj drugą liczbę'));
-    suma = a + b;
-    console.log('Suma podanych liczb wynosi: ' + suma);
-}
+do {
+    console.log(a);
+    a++;
+} while (a<=10);
