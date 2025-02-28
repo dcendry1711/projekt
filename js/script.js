ac1e906fca1865df8a174bf9177684688adae0f0
DanielCendry <d.cendry@sente.pl>
2025-02-28 18:58:37 +0100

    tablice

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
-let a = 1;
+//tablice
 
-do {
-    console.log(a);
-    a++;
-} while (a<=10);
+const samochody = ['audi','bmw','opel'];
+
+const samochody2 = ['polonez','skoda','vw'];
+
+// samochody.forEach((element) => console.log(element)); // iteracja po elementach tabeli
+
+const samochody3 = [
+    {
+        marka: "opel",
+        model: "astra",
+        przebieg: 120000,
+        cena: 10000,
+        ilosc: 3
+    },
+    {
+        marka: "mazda",
+        model: "5",
+        przebieg: 140000,
+        cena: 12000,
+        ilosc: 4 
+    }
+]
+
+// samochody3.forEach((element) => console.log(element));
+
+//wyswietl marke samochodu, która ma przebieg wiekszy niz 130k
+
+samochody3.forEach(function (e) {
+    if (e.przebieg>130000){
+        return console.log(e.marka);
+    }
+})
